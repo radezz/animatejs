56f3a52c6c111e5fbe05ce584d56ff6c5bc5883e
radezz <radoslaw.adamczyk@gmail.com>
2014-07-01 14:00:25 +0000

    changing closure library version

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     path: {
-      closure: 'node_modules/closure-library/',
+      closure: 'node_modules/obvious-closure-library/',
       src: 'src',
       tmp: 'tmp'
     },
